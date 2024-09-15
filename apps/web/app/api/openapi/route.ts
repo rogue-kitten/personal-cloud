b7e397acf85359191e605c71d37e36bde98d1fb0
nash <naash.works@gmail.com>
2024-09-15 18:21:51 +0530

    fix: build error

--- apps/web/app/api/openapi/route.ts
+++ apps/web/app/api/openapi/route.ts
@@ -1,6 +1,7 @@
 import { openApiDocument } from '@/server/api/openapi';
 import { NextResponse } from 'next/server';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function GET(req: Request) {
   return NextResponse.json(openApiDocument);
 }
---
+++ apps/web/migrations/0003_pink_domino.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "users" ADD COLUMN "name" text;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "image" text;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "emailVerified" timestamp;
