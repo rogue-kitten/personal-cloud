4ae950013ea6d1f54c05cd9d06e966bdd9f83c54
nash <naash.works@gmail.com>
2024-09-21 17:10:25 +0530

    fix: trpc handler to take care of preflight requests

--- apps/web/app/api/trpc/[trpc]/route.ts
+++ apps/web/app/api/trpc/[trpc]/route.ts
@@ -4,7 +4,16 @@ import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 import { cookies, headers } from 'next/headers';
 
 const handler = (request: Request) => {
-  console.log(`incoming request ${request.url}`);
+  if (request.method === 'OPTIONS') {
+    const res = new Response(null, { status: 200 });
+    res.headers.set('Access-Control-Allow-Origin', '*');
+    res.headers.set('Access-Control-Request-Method', '*');
+    res.headers.set('Access-Control-Allow-Methods', 'OPTIONS, GET');
+    res.headers.set('Access-Control-Allow-Headers', '*');
+    return res;
+  }
+
+  // console.log(`incoming request ${request.url}`);
   return fetchRequestHandler({
     endpoint: '/api/trpc',
     req: request,
